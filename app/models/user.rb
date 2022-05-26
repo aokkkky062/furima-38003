523ffcc9c3e5432f912abff2deb0187fbb966da0
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-05-27 00:13:21 +0900

    バリデーションの追加
    usersモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+      
+  with_options presence: true do
+    validates :nickname
+
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}   # パスワードは半角英数字混合の入力が必須 
+                         
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角(漢字・ひらがな・カタカナ)のみが使えます" } do
+      validates :last_name
+      validates :first_name
+    end
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "全角(カタカナ)のみが使えます" } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    validates :birthday
+  end
 end

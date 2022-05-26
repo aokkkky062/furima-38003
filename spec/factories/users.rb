8e0dd8e068ec81cd4fc34d187c6008f66921e1c6
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-05-27 03:27:56 +0900

    FactoryBotとFakerでランダム生成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.name }
+    email { Faker::Internet.unique.email }
+    password {'1a' + Faker::Internet.unique.password(min_length: 6)}
+    password_confirmation {password}
+    last_name { '春日' }
+    first_name { '俊彰' }
+    last_name_kana { 'カスガ' }
+    first_name_kana { 'トシアキ' }
+    birthday { Faker::Date.birthday }
+  end
+end

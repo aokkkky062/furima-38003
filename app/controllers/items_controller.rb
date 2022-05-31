52640bd451cd9eee8ee899dd4bace3287585ae15
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-05-31 23:45:27 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:Item).permit(:image, :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :days_of_ship_id, :price, )
+  end
 end

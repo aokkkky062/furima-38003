db7d54bd1737e05d36fcc78a48fac98b80361d6c
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-06-01 15:30:39 +0900

    ストロングパラメーターの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:Item).permit(:image, :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :days_of_ship_id, :price, )
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :days_of_ship_id, :price).merge(user_id: current_user.id)
   end
 end

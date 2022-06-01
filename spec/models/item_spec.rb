9103802c8878c2dff0f678319d8afeb64dce0d37
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-06-02 02:28:58 +0900

    itemモデルの単体テストコードを作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,127 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品登録' do
+    before do
+      @item = FactoryBot.create(:item)
+    end
+
+    context '商品出品登録ができる時' do
+      it '全ての入力事項が、存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it 'カテゴリーが「---」以外であれば登録できる' do
+        @item.category_id = 2
+        expect(@item).to be_valid
+      end
+      it '商品の状態が「---」以外であれば登録できる' do
+        @item.condition_id = 2
+        expect(@item).to be_valid
+      end
+      it '配送料の負担が「---」以外であれば登録できる' do
+        @item.charge_id = 2
+        expect(@item).to be_valid
+      end
+      it '発送元の地域が「---」以外であれば登録できる' do
+        @item.area_id = 2
+        expect(@item).to be_valid
+      end
+      it '発送までの日数が「---」以外であれば登録できる' do
+        @item.days_of_ship_id = 2
+        expect(@item).to be_valid
+      end
+      it '価格が半角数字でかつ300円〜9,999,999円であれば登録できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品登録ができない時' do
+      it 'ユーザー登録している人でないと出品できない' do
+        @item.user_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist', "User can't be blank")
+      end
+      it "画像がないと出品できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "商品名が空欄だと出品できない" do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "商品の説明が空欄だと出品できない" do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it "カテゴリー情報が空欄だと出品できない" do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "商品の状態の情報が空欄だと出品できない" do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it "配送料の負担の情報が空欄だと出品できない" do
+        @item.charge_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Charge can't be blank")
+      end
+      it "発送元の地域の情報が空欄だと出品できない" do
+        @item.area_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it "発送までの日数の情報が空欄だと出品できない" do
+        @item.days_of_ship_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days of ship can't be blank")
+      end
+      it "価格の情報が空欄だと出品できない" do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "価格が300円未満だと出品できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it "価格が10,000,000円以上だと出品できない" do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it 'カテゴリーの情報が「---」だと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Can't be blank")
+      end
+      it '商品の状態の情報が「---」だと出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition Can't be blank")
+      end
+      it '配送料の負担の情報が「---」だと出品できない' do
+        @item.charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Charge Can't be blank")
+      end
+      it '発送元の地域の情報が「---」だと出品できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area Can't be blank")
+      end
+      it '発送までの日数の情報が「---」だと出品できない' do
+        @item.days_of_ship_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days of ship Can't be blank")
+      end
+    end
+  end
 end

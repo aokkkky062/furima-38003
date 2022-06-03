61e7d5d1f61ae73bcd91750cfa5cc878bc4499cb
aokkkky062 <mcyzbnos062@docomo.ne.jp>
2022-06-03 19:30:38 +0900

    editとupdateアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-  before_action :set_item, only: :show
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -21,6 +21,23 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    if @item.user_id == current_user.id
+      @item = Item.find(params[:id])
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path
+    else
+      render 'edit'
+    end
   end
 
   private
